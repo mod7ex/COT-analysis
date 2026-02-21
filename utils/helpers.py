375f8c38ba9285d33390f65416bbcb727a30da5b
Mourad <mouradface1998@gmail.com>
2026-02-21 15:26:14 +0000

    save

--- utils/helpers.py
+++ utils/helpers.py
@@ -7,17 +7,17 @@ from .cot import cot_all, cot_year
 
 print_json = lambda data, i=8: print(json.dumps(data, indent=i))
 
-# ----------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
 
 csv_markets_path = lambda report_type: os.path.join(DATA_EXTRACTION_FOLDER_PATH, f"{report_type}-markets.csv")
 
 json_markets_path = lambda report_type: os.path.join(JSON_FOLDER_PATH, f"{report_type}-markets.json")
 
-# ----------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
 
 report_types_cols_path = lambda report_type: os.path.join(DATA_EXTRACTION_FOLDER_PATH, f"{report_type}-report_types_cols.txt")
 
-# ----------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def normalize_col(col: str):
     return (
@@ -35,6 +35,8 @@ def normalize_col(col: str):
 def normalize_cols(df):
     df.columns = [normalize_col(col.strip()) for col in df.columns]
     return df
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def get_markets(cot_report_type, save_csv=False, save_json=False):
     # Normalize column names
@@ -74,59 +76,7 @@ def get_report_type_cols(cot_report_type, save=False):
             for col in df.columns:
                 f.write(col + "\n")
 
-# def plot_df_chart(
-#         df = pd.DataFrame(),
-#         chart_title: str = "Chart Title",
-#         draw = True,
-#         save_to_html = False,
-#         save_file_name = "plot",
-#         use_markers= True
-# ):
-#     fig = px.line(df, markers=True)
-
-#     fig.update_layout(
-#         title=chart_title,
-#         template="plotly_dark",
-#         paper_bgcolor="black",
-#         plot_bgcolor="black",
-#         # width=1500,
-#         height=800,
-
-#         # hovermode="x unified",  # one vertical hover line
-#         dragmode="zoom",
-
-#         font=dict(color="white", size=12),
-
-#         legend=dict(
-#             itemclick="toggle",        # click = hide/show
-#             itemdoubleclick="toggleothers",
-#             bgcolor="rgba(0,0,0,0)"
-#         ),
-
-#         xaxis=dict(
-#             showgrid=True,
-#             gridcolor="rgba(255,255,255,0.08)",
-#             rangeslider=dict(visible=True),  # 🔥 range slider
-#         ),
-
-#         yaxis=dict(
-#             title='Contracts',
-#             showgrid=True,
-#             gridcolor="rgba(255,255,255,0.08)",
-#             fixedrange=False
-#         )
-#     )
-
-#     fig.update_traces(
-#         mode=f"lines{'+markers' if use_markers else ''}",
-#         line=dict(width=1),
-#         hovertemplate="<b>%{fullData.name}</b><br>%{y:.2f}<extra></extra>"
-#     )
-
-#     if save_to_html: fig.write_html(f"plots/{save_file_name}.html")
-
-#     if draw: return fig.show()
-#     else: return fig
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def plot_df_chart(
         df = pd.DataFrame(),
@@ -192,7 +142,7 @@ def plot_df_chart(
     else:
         return fig
 
-# ----------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def get_market_df(cot_report_type=TRADERS_IN_FINANCIAL_FUTURES_FUT, market=PICKED_MARKET):
 
